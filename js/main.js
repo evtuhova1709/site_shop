bb12b9ad334c20cf8531a8fc6db9721745e3e8aa
Anton Tolkachev <evtuhova1709@gmail.com>
2021-08-11 19:18:23 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+$('.slider_inner').slick({
+    nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
+    infinite:false
+});
+
+
+
+
+
+});
+
+
+
+
+
